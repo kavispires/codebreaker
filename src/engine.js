e483a3c051e5703c440395bf012e3a8997053026
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-05-07 23:42:46 -0700

    Added GameEngine

---
+++ src/engine.js
@@ -0,0 +1,15 @@
+import { shuffle, splitList } from './utils';
+import CARDS from './utils/cards';
+import { NUMBERS } from './utils/constants';
+
+class GameEngine {
+  constructor() {}
+
+  get state() {
+    return {};
+  }
+
+  reset() {}
+}
+
+export default new GameEngine();
