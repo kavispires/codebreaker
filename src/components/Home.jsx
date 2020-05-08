6931c1dc3418b0c58ac56b93799d705c9468cdf5
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-05-08 00:44:41 -0700

    Created Home component and setup functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormLabel from '@material-ui/core/FormLabel';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+
+import logo from '../images/codebreaker-logo.svg';
+
+import gameEngine from '../engine';
+import useGlobalState from '../useGlobalState';
+import { COLORS, SCREENS } from '../utils/constants';
 
 const Home = () => {
+  // Global States
+  const [, setGame] = useGlobalState('game');
+  const [, setScreen] = useGlobalState('screen');
+  // Local States
+  const [players, setPlayers] = useState(3);
+
+  const handleCreateGame = () => {
+    setGame(gameEngine.setup(players));
+    setScreen(SCREENS.GAME);
+  };
+
+  console.log(players);
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="home">
+      <img className="logo" src={logo} alt="Carto logo" />
+      <h1>Carto</h1>
+
+      <FormGroup>
+        <FormControl component="fieldset">
+          <FormLabel component="legend" style={{ color: COLORS.PRIMARY }}>
+            Player Count
+          </FormLabel>
+          <RadioGroup
+            aria-label="mode"
+            name="mode"
+            value={players}
+            onChange={(event) => setPlayers(+event.target.value)}
+          >
+            <FormControlLabel value={1} control={<Radio />} label="Solo" disabled />
+            <FormControlLabel value={2} control={<Radio />} label="2 players" />
+            <FormControlLabel value={3} control={<Radio />} label="3 players" />
+            <FormControlLabel value={4} control={<Radio />} label="4 players" />
+          </RadioGroup>
+        </FormControl>
+      </FormGroup>
+
+      <div className="home__actions">
+        <Button variant="outlined" className="btn btn--outline" onClick={handleCreateGame}>
+          Start Game
+        </Button>
+      </div>
     </div>
   );
 };
--- src/engine.js
+++ src/engine.js
@@ -3,13 +3,55 @@ import CARDS from './utils/cards';
 import { NUMBERS } from './utils/constants';
 
 class GameEngine {
-  constructor() {}
-
-  get state() {
-    return {};
+  constructor() {
+    this.playerCount = 0;
+    this.code = null;
+    this.hands = {};
+    this.questions = [];
   }
 
-  reset() {}
+  get state() {
+    return {
+      code: this.code,
+      hands: this.hands,
+    };
+  }
+
+  setup(playerCount) {
+    this.playerCount = playerCount;
+
+    const shuffledNumbers = shuffle(NUMBERS);
+
+    let playerHands = null;
+
+    if (playerCount < 4) {
+      playerHands = splitList(shuffledNumbers, 4);
+    } else {
+      playerHands = splitList(shuffledNumbers, 5);
+    }
+
+    if (playerCount > 2) {
+      this.code = playerHands.pop().sort();
+    }
+
+    this.hands = playerHands.reduce((acc, hand, index) => {
+      if (index < playerCount) {
+        acc[index + 1] = {
+          player: index + 1,
+          hand: hand.sort(),
+        };
+      }
+      return acc;
+    }, {});
+
+    this.questions = shuffle(CARDS);
+
+    return this.state;
+  }
+
+  reset() {
+    this.playerCount = 0;
+  }
 }
 
 export default new GameEngine();
