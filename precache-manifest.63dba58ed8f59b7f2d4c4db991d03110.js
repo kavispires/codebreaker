320428ebb3bd920403f5335e0c522155deba5146
Fabiano 'Kavis' Pires <kavispires@gmail.com>
2020-05-08 15:47:39 -0700

    Updates

---
+++ precache-manifest.63dba58ed8f59b7f2d4c4db991d03110.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c9a3d30d2b4357f7e76d0c058998c2",
+    "url": "/codebreaker/index.html"
+  },
+  {
+    "revision": "45d9d598b5761721305f",
+    "url": "/codebreaker/static/css/main.b99368f9.chunk.css"
+  },
+  {
+    "revision": "d75a6fc8e33b85db13ae",
+    "url": "/codebreaker/static/js/2.25902bb2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/codebreaker/static/js/2.25902bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45d9d598b5761721305f",
+    "url": "/codebreaker/static/js/main.f0f92b49.chunk.js"
+  },
+  {
+    "revision": "7784504f00a68fdf110f",
+    "url": "/codebreaker/static/js/runtime-main.54cb2e20.js"
+  },
+  {
+    "revision": "619b899aeb2145339a280b4b1cdb6c44",
+    "url": "/codebreaker/static/media/codebreaker-logo.619b899a.svg"
+  },
+  {
+    "revision": "8a19287fecfa5e584dfb0bfd33c6913e",
+    "url": "/codebreaker/static/media/rule-example-1.8a19287f.jpg"
+  },
+  {
+    "revision": "c7066c3e35ac6feef75bcb5e9e0e0a2d",
+    "url": "/codebreaker/static/media/rule-example-2.c7066c3e.jpg"
+  },
+  {
+    "revision": "5215b1457faa9c35a05ac5c3cd199073",
+    "url": "/codebreaker/static/media/rule-example-3.5215b145.jpg"
+  },
+  {
+    "revision": "02f95d584d7c1462965a50de5fe34f69",
+    "url": "/codebreaker/static/media/rule-example-4.02f95d58.jpg"
+  },
+  {
+    "revision": "fd7a64946598d9b888265b70853ace5e",
+    "url": "/codebreaker/static/media/rule-goal-4.fd7a6494.jpg"
+  },
+  {
+    "revision": "dd6c7c318597a3ff60fa9a3acc2672cb",
+    "url": "/codebreaker/static/media/rule-goal-5.dd6c7c31.jpg"
+  },
+  {
+    "revision": "6c34c973a6ade1b2bc751cf731a863df",
+    "url": "/codebreaker/static/media/rule-question.6c34c973.jpg"
+  },
+  {
+    "revision": "5dd2fa92aa1771d5945667bab26e5bae",
+    "url": "/codebreaker/static/media/rule-setup-4.5dd2fa92.jpg"
+  },
+  {
+    "revision": "3ea055bbce736e81e3648a005348bd25",
+    "url": "/codebreaker/static/media/rule-setup-5.3ea055bb.jpg"
+  },
+  {
+    "revision": "f55b24cbef6d781d2b2b35275fc27375",
+    "url": "/codebreaker/static/media/rule-setup-reveal.f55b24cb.jpg"
+  },
+  {
+    "revision": "2a46b9512e6e2a798c22347524e9dfd5",
+    "url": "/codebreaker/static/media/rule-turn.2a46b951.jpg"
+  }
+]);
